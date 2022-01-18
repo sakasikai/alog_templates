8a31a09d8dcd18eb0efabab950ca9c67f6ced35b
sakasikai <tonq_dlut@126.com>
2022-01-18 13:42:24 +0800

    add

---
+++ graph/Kruskal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Kruskal
+//
+//  Created by TonqMaI7 on 2022/1/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
